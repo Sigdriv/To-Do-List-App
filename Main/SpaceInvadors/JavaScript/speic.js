18170e032553abed528ad96454754f9d2fc199de
Kim Aleksander Persen <133632428+Hexcrow-k17@users.noreply.github.com>
2023-09-03 01:21:41 +0200

    finist vid beis game

--- Main/SpaceInvadors/JavaScript/speic.js
+++ Main/SpaceInvadors/JavaScript/speic.js
@@ -37,6 +37,13 @@
     let alienCount = 0; //number of aliens to defeat
     let alienVelocityx = 1; //alien moving speed
 
+    //bollets
+    let bulletArray = [];
+    let bulletVelocityy = -10; //bullet moving speed
+
+    let score = 0;
+    let gameOver = false;
+
     window.onload = function () {
         board = document.getElementById("board");
         board.width = boardWidth;
@@ -62,32 +69,94 @@
 
         requestAnimationFrame(update);
         document.addEventListener("keydown", moveShip);
+        document.addEventListener("keyup", shoot);
     }
 
     function update() {
         requestAnimationFrame(update);
 
-        context.clearRect(0, 0, board.width, board.height)
-
+        if (gameOver) {
+            return;
+        }
+    
+        context.clearRect(0, 0, board.width, board.height);
+    
         //ship
         context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
-
+    
         //aliens
         for (let i = 0; i < alienArray.length; i++) {
             let alien = alienArray[i];
             if (alien.alive) {
                 alien.x += alienVelocityx;
-
-                //if alien touvhes the borders
+    
+                //if alien touches the borders
                 if (alien.x + alien.width >= board.width || alien.x <= 0) {
-                    alienVelcityx *= -1;
+                    alienVelocityx *= -1;
+                    alien.x += alienVelocityx*2;
+    
+                    //move all aliens down by one row
+                    for (let j = 0; j < alienArray.length; j++) {
+                        alienArray[j].y += alienHeight;
+                    }
                 }
                 context.drawImage(alien.img, alien.x, alien.y, alien.width, alien.height);
+
+                if (alien.y >= ship.y) {
+                    gameOver = true;
+                    alert("Game Over");
+                }
             }
         }
+
+        //bullets
+        for (let i = 0; i < bulletArray.length; i++){
+            let bullet = bulletArray[i];
+            bullet.y += bulletVelocityy;
+            context.fillStyle = "red";
+            context.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+
+            //bullet collides with alien
+            for (let j = 0; j < alienArray.length; j++) {
+                let alien = alienArray[j];
+                if (!bullet.used && alien.alive && detectCollison(bullet, alien)) {
+                    bullet.used = true;
+                    alien.alive = false;
+                    alienCount--;
+                    score += 50;
+                }
+            }
+        }
+
+        //clear bullets
+        while (bulletArray.length > 0 && (bulletArray[0].used || bulletArray[0].y < 0)) {
+            bulletArray.shift(); //remove first element of the array
+        }
+
+        //next level
+        if (alienCount == 0) {
+            //increase the number of aliens in columns and rows
+            alienColumns = Math.min(alienColumns + 1, columns/2 -2); //cap at 16/2 -2 = 6
+            alienRows = Math.min(alienRows + 1, rows-4); //cap at 16 -4 = 12
+            alienVelocityx += 0.2; //increase alien speed
+            alienArray = []; //clear aliens
+            bulletArray = []; //clear bullets
+            createAliens();
+        }
+
+        //score
+        context.fillStyle="white";
+        context.font="16px courier";
+        context.fillText("Score: " + score, 5, 20);
+
     }
+    
 
     function moveShip(e) {
+        if (gameOver) {
+            return;
+        }
+
         if (e.code == "ArrowLeft" && ship.x - shipVelocityx >= 0) {
             ship.x -= shipVelocityx; //move left one tile
         }
@@ -111,4 +180,30 @@
             }
         }
         alienCount = alienArray.length;
+    }
+
+    function shoot(e) {
+        if (gameOver) {
+            return;
+        }
+
+        if (e.code == "Space") {
+            //shoot
+            let bullet = {
+                x : ship.x + ship.width*15/32,
+                y : ship.y,
+                width : tileSize/8,
+                height : tileSize/2,
+                used : false,
+            }
+            bulletArray.push(bullet);
+        }
+    }
+
+    function detectCollison(a, b) {
+        return a.x < b.x +b.width &&    //a's top left corner is to the left of b's top right corner
+                a.x + a.width > b.x &&  //a's top right corner passes b's top left corner
+                a.y < b.y + b.height &&     //a's top left corner doesn't reach b's bottom left corner
+                a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
+        
     }
