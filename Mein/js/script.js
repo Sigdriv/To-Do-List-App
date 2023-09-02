b9b2e6f118ac9fc0533cc2dd23c286bd13b097ae
Sigurd Drivstuen <sigdriv06+github@gmail.com>
2023-09-02 21:12:36 +0200

    Can press enter to add text to list

--- Mein/js/script.js
+++ Mein/js/script.js
@@ -2,6 +2,15 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+// Event listener for the input box
+inputBox.addEventListener('keyup', function (event) {
+    // Check if the enter key was pressed
+    if (event.keyCode === 13) {
+        // If enter key was pressed add a new task
+        addTask();
+    }
+});
+
 // Function to add a new task
 function addTask() {
     // Check if the input box is empty
